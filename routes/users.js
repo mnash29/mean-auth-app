f14bcad6ae440b1f0b93f1ab897c9aad79690f27
mnash29 <mtntrades10@gmail.com>
2019-06-13 23:14:16 +0000

    Added registration function commit

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
-// Register
-router.get("/register", (req, res, next) => {
-  res.send("REGISTER");
+const User = require("../models/users");
+
+// Register new user
+router.post("/register", (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+  
+  // Add user
+  User.addUser(newUser, (err, user) => {
+    if(err) {
+      res.json({success: false, msg: "Failed to register user"});
+    } else {
+      res.json({success: true, msg: "User registered sucessfully"});
+    }
+  });
 });
 
-// Authenticate
-router.get("/authenticate", (req, res, next) => {
+// Authenticate user
+router.post("/authenticate", (req, res, next) => {
   res.send("AUTHENTICATE");
 });
 
-// Profile
+// Get user profile
 router.get("/profile", (req, res, next) => {
   res.send("PROFILE");
-});
-
-// Validate
-router.get("/validate", (req, res, next) => {
-  res.send("VALIDATE");
 });
 
 module.exports = router;
