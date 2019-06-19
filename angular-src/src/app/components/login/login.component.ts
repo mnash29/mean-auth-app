75ee7114f62331b506aeafd11459f28e12149012
mnash29 <mtntrades10@gmail.com>
2019-06-19 16:08:58 -0700

    Added login component

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // Component properties
+  username: String;
+  password: String;
+  
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+    ) { }
 
   ngOnInit() {
   }
-
+  
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+    
+    this.authService.authenticateUser(user).subscribe(data => {
+      if((data as any).success) {
+        this.authService.storeUserData((data as any).token, (data as any).user);
+        this.flashMessage.show((data as any).msg, {cssClass: 'alert-danger', timeout: 3000});
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.flashMessage.show((data as any).msg, {cssClass: 'alert-danger', timeout: 5000});
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 }
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -16,4 +16,17 @@ export class AuthService {
     headers.append('ContentType', 'application/json');
     return this.http.post('http://54.218.118.181:8080/users/register', user, {headers: headers});
   }
+  
+  authenticateUser(user) {
+    let headers = new HttpHeaders();
+    headers.append('ContentType', 'application/json');
+    return this.http.post('http://54.218.118.181:8080/users/authenticate', user, {headers: headers});
+  }
+  
+  storeUserData(token, user) {
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
 }
