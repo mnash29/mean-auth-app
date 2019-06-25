e2e10b871f7e68544ad086f163022eb705463a94
mnash29 <mtntrades10@gmail.com>
2019-06-25 16:23:26 -0700

    Profile auth commit

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -22,12 +22,25 @@ export class AuthService {
     headers.append('ContentType', 'application/json');
     return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers});
   }
+
+  getProfile() {
+    let headers = new HttpHeaders();
+    this.loadToken();
+    headers.set('Authorization', this.authToken);
+    headers.set('ContentType', 'application/json');
+    return this.http.get('http://localhost:3000/users/profile', {headers: headers});
+  }
   
   storeUserData(token, user) {
     localStorage.setItem('id_token', token);
     localStorage.setItem('user', JSON.stringify(user));
     this.authToken = token;
     this.user = user;
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
   }
 
   logout() {
